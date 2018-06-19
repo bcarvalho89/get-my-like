f2966aa20c4de163781b359b8ba3c63f5364ae7e
Bruno Carvalho <bruno@brunocarvalho.me>
2018-06-19 09:07:16 -0300

    Add login buttons

--- src/containers/Dashboard.jsx
+++ src/containers/Dashboard.jsx
@@ -1,11 +1,62 @@
 import React, { Component } from 'react';
 
+import { auth, providerFaceboook, providerGoogle } from '../helpers/firebase';
+
 class Dashboard extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      user: null
+    };
+  }
+
+  componentDidMount() {
+    auth.onAuthStateChanged(user => {
+      if (user) {
+        this.setState({ user });
+      }
+    });
+  }
+
+  login = (provider) => {
+    auth.signInWithPopup(provider)
+      .then(result => {
+        const user = result.user;
+
+        this.setState({ user });
+      });
+  }
+
+  logout = () => {
+    auth.signOut()
+      .then(() => {
+        this.setState({
+          user: null
+        });
+      });
+  }
 
   render() {
     return (
       <div>
         <h1>Dashboard</h1>
+
+        <div>
+          <h4>Login</h4>
+
+          { this.state.user ?
+            <div>
+              <p>{ this.state.user.displayName || this.state.user.email }</p>
+              <button onClick={this.logout}>Sair</button>
+            </div> :
+            <div>
+              <button onClick={() => this.login(providerGoogle)}>Entrar com o Google</button>
+              <button onClick={() => this.login(providerFaceboook)}>Entrar com o Facebook</button>
+            </div>
+          }
+        </div>
       </div>
     )
   }
--- src/helpers/firebase.js
+++ src/helpers/firebase.js
@@ -3,6 +3,7 @@ import config from '../constants/config';
 
 firebase.initializeApp(config);
 
-export const provider = new firebase.auth.GoogleAuthProvider();
+export const providerGoogle = new firebase.auth.GoogleAuthProvider();
+export const providerFaceboook = new firebase.auth.FacebookAuthProvider();
 export const auth = firebase.auth();
 export default firebase;
